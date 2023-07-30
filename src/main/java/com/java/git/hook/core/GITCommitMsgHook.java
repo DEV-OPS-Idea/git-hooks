b25b4fb2b198196b61194267cc4af06589e29c1f
Sudipta Ghosh <1986.sudipta@gmail.com>
2023-07-30 23:46:57 +0530

    Completed check_issuenumber_in_begining_commit_message

--- src/main/java/com/java/git/hook/core/GITCommitMsgHook.java
+++ src/main/java/com/java/git/hook/core/GITCommitMsgHook.java
@@ -9,25 +9,43 @@ import java.util.List;
 
 public class GITCommitMsgHook {
 
-    public static boolean check_valid_commit(String currentBranches){
-        boolean blockedBranches=checkBlockedBranches(currentBranches);
-        if(blockedBranches){
-            System.out.println("ERROR: Unable to commit to branch "+currentBranches+". It is blocked for Commit. Please contact System Administrator.");
+    public static boolean check_valid_commit(String commitMessage, String currentBranches) {
+        boolean blockedBranches = checkBlockedBranches(currentBranches);
+        String projectId=null;
+        try {
+             projectId = new AppUtil().getValues("/config.properties", "JIRA_PROJECT_ID");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        return blockedBranches;
+        if (blockedBranches) {
+            System.out.println("ERROR: Unable to commit to branch " + currentBranches + ". It is blocked for Commit. Please contact System Administrator.");
+        }
+        boolean containsIssueNumber = check_issuenumber_in_begining_commit_message(commitMessage,projectId);
+        if (!containsIssueNumber) {
+            System.out.println("ERROR: Unable to commit as Commit Message should start with this pattern: "+projectId+"-1234 :[Message Description]");
+        }
+        return blockedBranches || !containsIssueNumber;
     }
-   public static boolean checkBlockedBranches(String currentBranches){
-       List<String> blockedBranchesList=null;
-       try {
-           String blockedBranches=new AppUtil().getValues("/config.properties","BLOCKED_BRANCHES");
-           blockedBranchesList = new ArrayList<String>(Arrays.asList(blockedBranches.split(",")));
-       } catch (IOException e) {
-           throw new RuntimeException(e);
-       }
-       if(blockedBranchesList!=null && blockedBranchesList.contains(currentBranches)) {
-           return true;
-       }else{
-           return false;
-       }
-   }
+
+    private static boolean check_issuenumber_in_begining_commit_message(String commitMessage, String projectId) {
+        boolean startWithIssueNumber;
+        String MESSAGE_REGEX = projectId + "-[0-9]+ :.*";
+        startWithIssueNumber = AppUtil.validRegex(MESSAGE_REGEX, commitMessage);
+        return startWithIssueNumber;
+    }
+
+    private static boolean checkBlockedBranches(String currentBranches) {
+        List<String> blockedBranchesList = null;
+        try {
+            String blockedBranches = new AppUtil().getValues("/config.properties", "BLOCKED_BRANCHES");
+            blockedBranchesList = new ArrayList<String>(Arrays.asList(blockedBranches.split(",")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (blockedBranchesList != null && blockedBranchesList.contains(currentBranches)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/main/java/com/java/git/hook/util/AppUtil.java
+++ src/main/java/com/java/git/hook/util/AppUtil.java
@@ -3,6 +3,8 @@ package com.java.git.hook.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class AppUtil {
     public String getValues(String propertyPath, String property) throws IOException {
@@ -12,5 +14,11 @@ public class AppUtil {
         String propertyValue = properties.getProperty(property);
         //System.out.println("propertyValue:: " + propertyValue);
         return propertyValue;
+    }
+    public static boolean validRegex(String pattern,String message){
+        Pattern patten = Pattern.compile(pattern);
+        Matcher matcher = patten.matcher(message);
+        boolean validMessage = matcher.matches();
+        return validMessage;
     }
 }
