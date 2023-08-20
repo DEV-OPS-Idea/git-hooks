b34038ec6bb027694185ff661ec0f4cfbacafa5c
SUDIPTA GHOSH <1986.sudipta@gmail.com>
2023-08-21 00:35:22 +0530

    Commit Message

--- src/main/java/com/java/git/hook/constant/JiraConstant.java
+++ src/main/java/com/java/git/hook/constant/JiraConstant.java
@@ -1,7 +1,7 @@
 package com.java.git.hook.constant;
 
 public class JiraConstant {
-    public static final String JIRA_URL ="" ;
-    public static String username;
-    public static String password;
+    public static final String JIRA_URL="https://sudiptaflowable1.atlassian.net/";
+    public static String username="sudipta.flowable.1@gmail.com";
+    public static String password="ATATT3xFfGF0pO7OkuAW34dhmZW4AfZ80jFOdmJnZSduCr968tiyp7yNekWqKx1IUnRkBcuCop_yOdW2xP4pvJiL2JXMPr3_yp4t3bF9rpj2I5jFlzSK2YAlpXdylnLyps9wZ7WS2Sbqbjvjokf0h3YKWU-lpBYgu3OBZlTVkQXUkgzckWCrAIo=513D62C6";
 }
